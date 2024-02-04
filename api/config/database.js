3d8a4f0950e3f5a12b6fca33fa341ad666f7e7f6
DevSachin-09 <bharbeysachin@gmail.com>
2024-02-04 16:10:47 +0530

    fixes

--- api/config/database.js
+++ api/config/database.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose')
 
 require('dotenv').config()
 
-<<<<<<< HEAD
-exports.connect = async () => {
-    await mongoose.connect(process.env.MONGO)
-=======
 exports.connect = () => {
     mongoose.connect(process.env.MONGO)
->>>>>>> 30f7cc72e0fc86405ce75d5a0f9d143874482e1d
     .then(
         console.log("Database connection successfully")
     )
