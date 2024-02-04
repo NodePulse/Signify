a8ac0b5165c757ec499ec9708b116202eedca854
DevSachin-09 <bharbeysachin@gmail.com>
2024-02-04 17:01:17 +0530

    creating signup api route

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,20 @@
+const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+
+const signUp = async (req, res) => {
+    const { username, email, password } = req.body
+
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+    const newUser = new User({ username, email, password: hashedPassword })
+    try {
+        await newUser.save()
+        res.status(201).json({
+            message: "User created successfully"
+        })
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports = signUp
--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const userRoutes = require('./routes/user.route')
+const authRoutes = require('./routes/auth.route')
 
 const app = express()
+app.use(express.json())
 
 require('./config/database').connect()
 
@@ -16,3 +18,4 @@ app.listen(3000, () => {
 // })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
