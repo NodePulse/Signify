7ba2cb61cd21b077b8df8875cb9e4f58b9584a6b
DevSachin-09 <bharbeysachin@gmail.com>
2024-02-05 15:38:04 +0530

    adding signin functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
 
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
 
   function changeHandler(e) {
     setFormData({ ...formData, [e.target.id]: e.target.value })
@@ -30,6 +31,7 @@ export default function SignUp() {
         setError(true)
         return
       }
+      navigate('/signin')
     } catch (error) {
       setLoading(false)
       setError(false)
